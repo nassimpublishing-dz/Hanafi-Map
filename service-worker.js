44c364b9d8f5ce81fd8d3809944c24a5598a5ee4
nassimpublishing-dz <nassimpublishing@gmail.com>
2025-11-17 13:29:36 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,32 @@
-// service-worker.js simple
-self.addEventListener('install', evt => self.skipWaiting());
-self.addEventListener('activate', evt => self.clients.claim());
-self.addEventListener('fetch', evt => {
-  evt.respondWith(fetch(evt.request).catch(() => caches.match(evt.request)));
+// service-worker.js
+
+const CACHE_NAME = 'hanafi-map-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/js/firebase-config.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
