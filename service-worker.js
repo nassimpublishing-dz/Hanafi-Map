43072ec4e4ca5b1a37d678ebacdec067f8a1cbfe
nassimpublishing-dz <nassimpublishing@gmail.com>
2025-11-18 10:38:22 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,34 @@
 // Nom du cache
-const CACHE_NAME = 'hanafi-map-v1';
+const CACHE_NAME = 'hanafi-map-v2';
 
-// Fichiers à mettre en cache
+// Fichiers à mettre en cache (CHEMINS CORRIGÉS)
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/service-worker.js',
-  '/css/styles.css',
-  '/js/app.js',
-  '/js/firebase-config.js',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/Hanafi-Map/',
+  '/Hanafi-Map/index.html',
+  '/Hanafi-Map/manifest.json',
+  '/Hanafi-Map/service-worker.js',
+  '/Hanafi-Map/app.js',
+  '/Hanafi-Map/icon-192.png',
+  '/Hanafi-Map/icon-512.png',
+  '/Hanafi-Map/magasin-delectronique.png',
+  '/Hanafi-Map/camion-dexpedition.png'
 ];
 
-// Installation
+// Installation AVEC GESTION D'ERREUR
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache ouvert, ajout des fichiers...');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(error => {
+        console.log('Erreur cache:', error);
+      })
   );
 });
 
-// Activation
+// Activation (reste identique)
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -37,7 +43,7 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Interception des requêtes
+// Interception des requêtes (reste identique)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
